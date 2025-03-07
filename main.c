8edd7e9aa52c7388f9660f9a2583cd047fc85988
djdylex <dominicnewman022@gmail.com>
2025-03-07 00:15:04 +0000

    developed main

--- main.c
+++ main.c
@@ -1,5 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 
-int main() {
+void sigchild_handler(int s) {
+	int existing_err = errno;
+
+	while (waitpid(-1, NULL, WNOHANG) > 0);
+
+	errno = saved_errno;
+}
+
+int main(void) {
+	int sockfd, newFd;
+	struct addrinfo hints, *servinfo, *p;
+	struct sockaddr_storage their_addr;
+	socklen_t sinSize;
+	struct sigaction sa;
+	int yes = 1;
+	char s[INET6_ADDRSTRLEN];
+	int rv;
+
+	memset(&hints, 0, sizeof hints)
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+
+	if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+		fprintf(stderr, "addr info error: %s\n", gai_strerror(rv));
+		return 1;
+	}
+
 	
 }
